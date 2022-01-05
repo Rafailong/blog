e976fcf242f6843fe5f14d555f9cc7c4a680bf1a
Rafa Avila <rafa.avim@gmail.com>
2022-01-05 13:47:06 -0600

    includes scalacheck  mdoc scripts

--- mdoc_watch.sh
+++ mdoc_watch.sh
@@ -2,4 +2,5 @@
 cs launch org.scalameta:mdoc_2.13:2.2.24 -- \
   --site.VERSION 1.0.0 \
   --in src_scala/$1 \
-  --watch
+  --watch \
+  --classpath $(coursier fetch -p org.scalacheck:scalacheck_2.13:1.15.4)
